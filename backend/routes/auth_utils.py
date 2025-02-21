450c29a3e27313bfc1960d872da81bf251db704f
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2025-02-21 15:08:02 +0300

    Update auth_utils.py

--- backend/routes/auth_utils.py
+++ backend/routes/auth_utils.py
@@ -43,3 +43,5 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         raise credentials_exception
     return user
 
+    user.role = role  # Attach role to user object
+    return user
