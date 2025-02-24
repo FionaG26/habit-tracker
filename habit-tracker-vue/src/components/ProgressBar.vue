475ec75318c853ce48ba744ee1ea47bec9503f38
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2025-02-24 12:20:06 +0300

    Update ProgressBar.vue

--- habit-tracker-vue/src/components/ProgressBar.vue
+++ habit-tracker-vue/src/components/ProgressBar.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="progress-container">
-    <p>Habits Completed: {{ completedHabits }}/{{ totalHabits }}</p>
-    <div class="progress-bar" :style="{ width: progressPercentage + '%' }"></div>
+    <p class="mb-2">Habits Completed: {{ completedHabits }}/{{ totalHabits }}</p>
+    <div class="progress-bar-container">
+      <div class="progress-bar" :style="{ width: progressPercentage + '%' }"></div>
+    </div>
   </div>
 </template>
 
@@ -24,14 +26,21 @@ export default {
 <style scoped>
 .progress-container {
   margin: 20px 0;
-  height: 20px;
-  background: #ddd;
+  text-align: center;
+}
+
+/* Progress Bar */
+.progress-bar-container {
+  width: 100%;
+  height: 10px;
+  background: rgba(255, 255, 255, 0.2);
   border-radius: 5px;
   overflow: hidden;
 }
+
 .progress-bar {
   height: 100%;
-  background: #28a745;
+  background: #4ade80;
+  transition: width 0.5s ease-in-out;
 }
 </style>
-
