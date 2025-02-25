c93df6e063250a318e04990ced284d12502a3b8e
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2025-02-25 14:48:41 +0300

    Update auth.py

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -5,7 +5,7 @@ from models import User
 from schemas import UserCreate, UserLogin, UserResponse, TokenResponse
 from database import get_db
 from passlib.context import CryptContext
-from .auth_utils import create_access_token, verify_password, get_current_user, get_password_hash as hash_password
+from .auth_utils import create_access_token, create_refresh_token, verify_password, get_current_user, get_password_hash as hash_password
 from authlib.integrations.starlette_client import OAuth
 from dotenv import load_dotenv
 from datetime import timedelta, datetime
