f182ee8fd9f5248d3c24f875cbaa17f64b0f7c63
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2025-02-27 12:45:26 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
